84f964c99bb3a4b856ec04e777213cb4dc49f27c
moonknight324 <141109828+moonknight324@users.noreply.github.com>
2023-11-21 09:25:54 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,29 @@
+// Part I
+// const container = document.getElementById("react-container");
+// ReactDOM.render("Hello! Welcome to React",container);
+
+
+// //Part II
+// // Functional component
+// const Container = () =>{
+//     return React.createElement(`div`,null,`Hey Kalvians! Welcome to React Learning`,
+//        React.createElement(`div`,null,`Let's rock and roll`)
+//        );
+// }
+// // Render the Container
+// ReactDOM.render(React.createElement(Container),container);
+
+
+// // Part III
+// class ReactContainer extends React.Component{
+//       render(){
+//           return React.createElement(`div`,null,`Hey Kalvians`,
+//           React.createElement(`div`,null,`Let's rock and roll with classes`)
+//           );
+//       }
+    
+//   }
+// const container = document.getElementById('react-container');
+// ReactDOM.render(React.createElement(ReactContainer),container);
+
+  
